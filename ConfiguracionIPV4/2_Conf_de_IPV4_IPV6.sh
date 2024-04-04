a72e8b4f8f46c704afb83741270bde66ba7e8175
Nois2 <103067863+Nois2@users.noreply.github.com>
2024-04-03 22:08:03 -0600

    Arreglando el problema de 2_conf_ip

--- ConfiguracionIPV4/2_Conf_de_IPV4_IPV6.sh
+++ ConfiguracionIPV4/2_Conf_de_IPV4_IPV6.sh
@@ -10,15 +10,20 @@ echo
 
 # Contenido para agregar al archivo 00-installer-config.yaml
 network_config='
-(This is the network config written by 'subiquity'
+(#This is the network config written by '"'"'subiquity'"'"'
 network:
   version: 2
   rendered: networkd
   ethernets:
+    enp0s3:
+      dhcp4: true
+      dhcp6: true
+      optional: true
+
     enp0s8:
-      addresses: [172.16.0.2/24, 172.16.0.3/24, '2001:DB7:DEA:A::2/64', '2001:DB7:DEA:A::3/64']
+      addresses: [172.16.0.2/24, 172.16.0.3/24, '"'"'2001:DB7:DEA:A::2/64'"'"', '"'"'2001:DB7:DEA:A::3/64'"'"']
       nameservers:
-        addresses: [172.16.0.2, '2001:DB7:DEA:A::2', '2001:4860:4860::8888']
+        addresses: [172.16.0.2, '"'"'2001:DB7:DEA:A::2'"'"', '"'"'2001:4860:4860::8888'"'"']
       dhcp4: false
       dhcp6: false
       optional: true)
@@ -26,7 +31,7 @@ network:
 
 # Escribe el contenido en el archivo 00-installer-config.yaml
 echo "Escribiendo en el archivo 00-installer-config.yaml..."
-echo "$network_config" > 00-installer-config.yaml
+echo "$network_config" >00-installer-config.yaml
 
 # Verifica si la escritura fue exitosa
 if [ $? -eq 0 ]; then
