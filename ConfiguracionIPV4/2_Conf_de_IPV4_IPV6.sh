67977d727bdc54d7aa69a77b1e976a7f1adfbacd
Nois2 <103067863+Nois2@users.noreply.github.com>
2024-04-03 20:44:55 -0600

    Aquí agrege configuracion de ipv4, ipv6

---
+++ ConfiguracionIPV4/2_Conf_de_IPV4_IPV6.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Cambia al directorio /etc/netplan
+cd /etc/netplan || exit 1
+
+# Muestra los archivos del directorio /etc/netplan
+echo "Archivos en el directorio /etc/netplan:"
+ls
+echo
+
+# Contenido para agregar al archivo 00-installer-config.yaml
+network_config='
+(This is the network config written by 'subiquity'
+network:
+  version: 2
+  rendered: networkd
+  ethernets:
+    enp0s8:
+      addresses: [172.16.0.2/24, 172.16.0.3/24, '2001:DB7:DEA:A::2/64', '2001:DB7:DEA:A::3/64']
+      nameservers:
+        addresses: [172.16.0.2, '2001:DB7:DEA:A::2', '2001:4860:4860::8888']
+      dhcp4: false
+      dhcp6: false
+      optional: true)
+'
+
+# Escribe el contenido en el archivo 00-installer-config.yaml
+echo "Escribiendo en el archivo 00-installer-config.yaml..."
+echo "$network_config" > 00-installer-config.yaml
+
+# Verifica si la escritura fue exitosa
+if [ $? -eq 0 ]; then
+    echo "El contenido se ha agregado correctamente al archivo 00-installer-config.yaml."
+else
+    echo "Error: No se pudo agregar el contenido al archivo 00-installer-config.yaml." >&2
+fi
